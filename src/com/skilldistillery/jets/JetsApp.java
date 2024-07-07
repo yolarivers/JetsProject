7279dfe4b601c873644f7a28860091dc45a4594f
Yola Rivers <yolarivers@gmail.com>
2024-07-07 14:16:04 -0500

    user story 1 is complete

--- src/com/skilldistillery/jets/JetsApp.java
+++ src/com/skilldistillery/jets/JetsApp.java
@@ -7,6 +7,12 @@ public class JetsApp {
 	private Scanner keyboard = new Scanner (System.in);
 	
 	public static void main (String[ ] args) {
+		
+	}
 
+		public void launch() {
+		       
+	        System.out.println("Buckle up and enjoy the flight!");
+			
 }
 }
